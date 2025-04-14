6cdf7f66011b478bb75a171f0a1d439f9012b16a
EmilseCampanelli <emicampanelli16@gmail.com>
2025-04-14 01:23:48 -0300

    changes in program

---
+++ APIAUTH.Data/ActivesWContextFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace APIAUTH.Data.Context
+{
+    public class ActivesWContextFactory : IDesignTimeDbContextFactory<ActivesWContext>
+    {
+        public ActivesWContext CreateDbContext(string[] args)
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile($"appsettings.json", optional: false)
+                .AddJsonFile($"appsettings.{environment}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var connectionString =
+                Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection") ??
+                configuration.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<ActivesWContext>();
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+
+            return new ActivesWContext(optionsBuilder.Options);
+        }
+    }
+}
