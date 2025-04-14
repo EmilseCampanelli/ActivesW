7fd2e1b19ae185d4cf04453d9c7aab9ce2678d3c
EmilseCampanelli <emicampanelli16@gmail.com>
2025-04-14 00:54:45 -0300

    changes in program

--- APIAUTH.Server/Program.cs
+++ APIAUTH.Server/Program.cs
@@ -84,6 +84,12 @@ builder.Services.AddSignalR();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<ActivesWContext>();
+    context.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
